5cb8784574499a6b82a8210a6b1ad6b19ca99941
Echen-prog <65425548+Echen-prog@users.noreply.github.com>
2020-08-20 13:11:58 -0700

    start

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 //import App from './App';
-import Hello from './Hello';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
+import Card from "./card";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { robots } from "./robots";
 
 ReactDOM.render(
-  <React.StrictMode>
-  	{/*< App/>*/}
-    <Hello/>,
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <React.StrictMode>
+      {/*< App/>*/}
+      <Card id={robots[0].id} name={robots[0].name} quote={robots[0].quote} />,
+      <Card id={robots[1].id} name={robots[1].name} quote={robots[1].quote} />,
+      <Card id={robots[2].id} name={robots[2].name} quote={robots[2].quote} />,
+      <Card id={robots[3].id} name={robots[3].name} quote={robots[3].quote} />,
+    </React.StrictMode>
+  </div>,
+  document.getElementById("root")
 );
-
-
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Meow Zedong",
+    username: "kommunist",
+    quote: "OUR properties",
+  },
+  {
+    id: 2,
+    name: "Chang Kai-Shrek",
+    username: "nationalist",
+    quote: "it's free real estate",
+  },
+  {
+    id: 3,
+    name: "",
+    username: "",
+    quote: "",
+  },
+  {
+    id: 4,
+    name: "",
+    username: "",
+    quote: "",
+  },
+  {
+    id: 5,
+    name: "",
+    username: "",
+    quote: "",
+  },
+];
